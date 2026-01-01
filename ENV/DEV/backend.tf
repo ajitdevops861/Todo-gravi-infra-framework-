2cc4e19b2bdecbbc40a3e13355c27c8aa0afb31f
ajitdevops861 <ajitraghav397@gmail.com>
2026-01-01 17:14:15 +0530

    updated backend

--- ENV/DEV/backend.tf
+++ ENV/DEV/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "shiviaj101"
-    storage_account_name = "stgshivi101"
-    container_name       = "tfstate"
+    storage_account_name = "stgshivi101" // storage
+    container_name       = "tfstate" // container
     key                  = "dev2.terraform.tfstate" # for dev 
     use_azuread_auth     = false                    # backend
   }
